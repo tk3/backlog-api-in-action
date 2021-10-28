e7196127063895f8d44906e0e84c34cdb135a5e5
tk3 <tk3inc@gmail.com>
2021-10-28 22:42:53 +0900

    add file

---
+++ 04_print_api_limit_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from dotenv import load_dotenv
+import os
+import requests
+
+
+def print_api_limit_1():
+    backlog_url = os.getenv('BACKLOG_URL')
+    api_key = os.getenv('BACKLOG_API_KEY')
+
+    r = requests.get(
+        backlog_url + '/api/v2/users/myself',
+        params={
+            'apiKey': api_key
+        })
+
+    print('X-RateLimit-Limit: ' + r.headers['X-RateLimit-Limit'])
+    print('X-RateLimit-Remaining: ' + r.headers.get('X-RateLimit-Remaining'))
+    print('X-RateLimit-Reset: ' + r.headers.get('X-RateLimit-Reset'))
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    print_api_limit_1()
